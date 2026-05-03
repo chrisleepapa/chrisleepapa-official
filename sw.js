87c631ad1c7698e86a8e3a574ce6d9b18014be91
chrisleepapa <myeongheon.mung@gmail.com>
2026-05-04 00:37:35 +0900

    sw.js 업데이트

--- sw.js
+++ sw.js
@@ -1,8 +1,19 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+// 서비스 워커 설치 이벤트
+self.addEventListener('install', (event) => {
+  console.log('ChrisPapa Service Worker: Installed');
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  // 웹페이지 요청 시 로직 (기본 설정)
-  e.respondWith(fetch(e.request));
+// 서비스 워커 활성화 이벤트
+self.addEventListener('activate', (event) => {
+  console.log('ChrisPapa Service Worker: Activated');
+});
+
+// 웹 페이지 요청 시 처리 (네트워크 우선 전략)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('인터넷 연결이 원활하지 않습니다.');
+    })
+  );
 });

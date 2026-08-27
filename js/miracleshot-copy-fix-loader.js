42beeb6e5d9a8b7338dc54ecedbeefb4b6a1da09
chrisleepapa <myeongheon.mung@gmail.com>
2026-08-27 18:52:01 +0900

    Add Miracle Shot copy fix loader

---
+++ js/miracleshot-copy-fix-loader.js
@@ -0,0 +1,6 @@
+// Loads the Miracle Shot copy-only correction without changing page content or layout.
+(function () {
+  const script = document.createElement('script');
+  script.src = '/js/miracleshot-copy-fix.js';
+  document.head.appendChild(script);
+})();

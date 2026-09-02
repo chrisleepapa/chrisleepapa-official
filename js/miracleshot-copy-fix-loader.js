21b8fde9f3405c8811bab07007bfee4db9a15e96
chrisleepapa <myeongheon.mung@gmail.com>
2026-09-02 14:23:12 +0900

    chore: remove unused Miracle Shot fix loader

--- js/miracleshot-copy-fix-loader.js
+++
@@ -1,6 +0,0 @@
-// Loads the Miracle Shot copy-only correction without changing page content or layout.
-(function () {
-  const script = document.createElement('script');
-  script.src = '/js/miracleshot-copy-fix.js';
-  document.head.appendChild(script);
-})();
